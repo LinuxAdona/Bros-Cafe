cfae289a5d4cd97423e5ad9c64339de8867d4051
Linux Adona <linuxadona17@gmail.com>
2025-11-14 01:02:28 +0800

    fix: hope it works

--- src/services/functions.php
+++ src/services/functions.php
@@ -53,7 +53,7 @@ function requireRole($role)
     preventCache();
     requireLogin();
     if (!hasRole($role)) {
-        header('Location: /home2/broscafe/public_html/401.html');
+        header('Location: /home2/broscafe/public_html/401.shtml');
         exit();
     }
 }
@@ -64,7 +64,7 @@ function requireEmployee()
     preventCache();
     requireLogin();
     if (!isEmployee()) {
-        header('Location: /home2/broscafe/public_html/401.html');
+        header('Location: /home2/broscafe/public_html/401.shtml');
         exit();
     }
 }
